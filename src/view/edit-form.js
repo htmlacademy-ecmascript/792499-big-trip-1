309f1e897cdcc405be7df1631550182238f7baaa
anatoly <anatolijdegtyar@yandex.ru>
2024-06-19 07:01:18 +0300

    Добавляет фото и описание в форму редактирования

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -4,7 +4,9 @@ import {humanizePointDueDate} from './../utils.js';
 const createEditPoint = (point) => {
   const {basePrice, event, img, destination, offer, dateFrom, dateTo} = point;
   const {offers} = offer;
+  const {description, pictures} = destination;
 
+  const createImgMarkup = (dataMarkup) => Object.entries(dataMarkup).map(([, value]) => `<img class="event__photo" src="${value.src}.jpg" alt="${value.description}">`).join('');
   const createMarkup = (dataMarkup) => Object.entries(dataMarkup).map(([, value]) => `
       <div class="event__offer-selector">
         <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-${value.ID}" type="checkbox" name="${value.TITLE}" checked>
@@ -118,15 +120,11 @@ const createEditPoint = (point) => {
 
               <section class="event__section  event__section--destination">
                 <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+                <p class="event__destination-description">${description}</p>
 
                 <div class="event__photos-container">
                   <div class="event__photos-tape">
-                    <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-                    <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-                    <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-                    <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-                    <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
+                    ${createImgMarkup(pictures)}
                   </div>
                 </div>
               </section>
